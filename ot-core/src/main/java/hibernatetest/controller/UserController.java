93f0095f8f144dbb15893c0a5cc417a766aae334
fyd <2221918807@qq.com>
2017-09-26 18:03:05 +0800

    2017年9月26日18:03:02

--- ot-core/src/main/java/hibernatetest/controller/UserController.java
+++ ot-core/src/main/java/hibernatetest/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/user")
-@Scope("prototype")
+@Scope("prototype") //多例模式
 public class UserController {
 
     @RequestMapping(value = "/save")
